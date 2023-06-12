40321939e4cde8bbd96be2b725be050dbdf9a570
k0baya <132335755+k0baya@users.noreply.github.com>
2023-06-12 17:08:18 +0800

    Update test.sh

--- test.sh
+++ test.sh
@@ -12,8 +12,8 @@ mkdir -p program && mkdir -p tempupdate
 
 tar -xzf manager.tar.gz -C tempupdate && rm -f manager.tar.gz && mv -f tempupdate/* program/manager && rm -rf tempupdate && cd program && chmod +x manager && cd /home/runner/${REPL_SLUG}
 
-if [ -s main.sh ]; then
-    sed -i '1s/.*/output=$(.\/background\/program\/manager admin) \&\& username=$(echo "$output" | grep -oP '\''username: \\K\\S+'\'') \&\& password=$(echo "$output" | grep -oP '\''password: \\K\\S+'\'') \&\& echo "username: $username" > user_info.txt \&\& echo "password: $password" >> user_info.txt \&\& nohup .\/background\/program\/manager >\/dev\/null 2>\&1/' main.sh
+if [ -s "main.sh" ]; then
+    echo "nohup ./background/program/manager admin > /home/runner/\${REPL_SLUG}/templog.txt && sed -n -e 's/username: \(.*\)/username: \1/p' -e 's/password: \(.*\)/password: \1/p' /home/runner/\${REPL_SLUG}/templog.txt > user_info.txt && rm /home/runner/\${REPL_SLUG}/templog.txt && nohup ./background/program/manager server >/dev/null 2>&1" > main.sh
 else
-    echo 'output=$(./background/program/manager admin) && username=$(echo "$output" | grep -oP '\''username: \\K\\S+'\'') && password=$(echo "$output" | grep -oP '\''password: \\K\\S+'\'') && echo "username: $username" > user_info.txt && echo "password: $password" >> user_info.txt && nohup ./background/program/manager >/dev/null 2>&1' > main.sh
+    echo "nohup ./background/program/manager admin > /home/runner/\${REPL_SLUG}/templog.txt && sed -n -e 's/username: \(.*\)/username: \1/p' -e 's/password: \(.*\)/password: \1/p' /home/runner/\${REPL_SLUG}/templog.txt > user_info.txt && rm /home/runner/\${REPL_SLUG}/templog.txt && nohup ./background/program/manager server >/dev/null 2>&1" > main.sh
 fi
