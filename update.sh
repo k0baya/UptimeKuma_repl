74dc05e5f825ec654c611010a7bd5a57d65895c2
k0baya <132335755+k0baya@users.noreply.github.com>
2023-10-01 17:32:20 +0800

    Update update.sh

--- update.sh
+++ update.sh
@@ -1,1 +1,4 @@
-git clone https://github.com/louislam/uptime-kuma.git && mv -b uptime-kuma/* ./ && mv -b uptime-kuma/.[^.]* ./ && rm -rf *~ && rm -rf uptime-kuma && rm -rf README.md && rm -rf .git && npm ci --production && npm run download-dist && echo 'require("./server/server.js");' > index.js && sed -i 's/.*/require(".\/server\/server.js");/' index.js
+wget -O kuma.zip https://github.com/louislam/uptime-kuma/archive/refs/tags/1.21.3.zip && unzip kuma.zip
+mv -b uptime-kuma-1.21.3/* ./ && mv -b uptime-kuma-1.21.3/.[^.]* ./ && rm -rf *~ && rm -rf uptime-kuma-1.21.3 && rm -rf README.md
+npm ci --production && npm run download-dist
+echo 'require("./server/server.js");' > index.js
